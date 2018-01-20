3dcde8da9483c72ad260952d1b5132db9124411a
Rasmus Toftdahl Olesen <halfdan@halfdans.net>
2018-01-20 16:23:35 +0100

    Also extract information about the external unit (slave 1)

--- extract.py
+++ extract.py
@@ -25,7 +25,7 @@ if '--gry' in argv:
 USERNAME = argv[1]
 PASSWORD = argv[2]
 
-ALLOWED_UNITS = ['%', '°C', 'ºC']
+ALLOWED_UNITS = ['%', '°C', 'ºC', 'bar', 'Hz', 'h']
 
 def send_gry ( source_name, value ):
     message = '%s %s' % (source_name, value)
@@ -34,28 +34,45 @@ def send_gry ( source_name, value ):
     sock.sendto(message.encode('utf-8'), (GRY_HOST, GRY_UDP_PORT))
     sock.close()
 
-items = []
+def is_float(txt):
+    try:
+        float(txt)
+        return True
+    except ValueError:
+        return False
+
+items = {}
 with requests.Session() as s:
     s.get ( 'https://www.nibeuplink.com/Welcome' )
     s.post ( 'https://www.nibeuplink.com/Login', data = {'Email': USERNAME, 'Password': PASSWORD} )
     s.get ( 'https://www.nibeuplink.com/Language/en-GB' )
-    r = s.get ( 'https://www.nibeuplink.com/System/43106/Status/ServiceInfo/0' )
-    open ( 'test.html', 'w' ).write( r.text )
+    for nibeIndex in range(2):
+        r = s.get ( 'https://www.nibeuplink.com/System/43106/Status/ServiceInfo/{0}'.format(nibeIndex) )
+        open ( 'test.html', 'w' ).write( r.text )
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    for table in soup.find_all('table'):
-        for tr in table.find_all('tr'):
-            tds = tr.find_all('td')
-            if len(tds) == 2:
-                items.append ( (next(tds[0].stripped_strings), tds[1].span.text) )
+        for table in soup.find_all('table'):
+            for tr in table.find_all('tr'):
+                tds = tr.find_all('td')
+                if len(tds) == 2:
+                    name = next(tds[0].stripped_strings)
+                    value = tds[1].span.text
+                    if name not in items:
+                        items[name] = value
 
-for key, value in items:
+
+for key, value in items.items():
+    name = 'NIBE.' + key.replace(' ', '_').replace('.', '')
+    fvalue = None
     for unit in ALLOWED_UNITS:
         if value.endswith(unit):
-            name = 'NIBE.' + key.replace(' ', '_').replace('.', '')
             fvalue = float(value[:-(len(unit))])
-            if GRY_HOST:
-                send_gry ( name, fvalue )
-            else:
-                print ( name, fvalue )
+    if fvalue is None and is_float(value):
+        fvalue = float(value)
+
+    if fvalue:
+        if GRY_HOST:
+            send_gry ( name, fvalue )
+        else:
+            print ( name, fvalue )
